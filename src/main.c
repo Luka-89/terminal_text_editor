0e3f8ab756ccc7a178f2157a5472de5ff60e7eab
Luka Batricevic <lukabatricevic8989@gmail.com>
2026-02-06 21:00:43 +0100

    Add disabling and enabling echo in enableRawMode and disableRawMode

--- src/main.c
+++ src/main.c
@@ -1,7 +1,34 @@
-#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+struct termios orig_termios;
+
+void disableRawMode() {
+    //sets the attributes back to the original
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode() {
+    //puts the attributes into struct original termios
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios;
+
+    //c_lflag - 0s and 1s representing local controller flags
+    raw.c_lflag &= ~(ECHO);
+
+    //sets the attributes from raw
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 
 int main() {
-    printf("Hello world\n");
-    
+    enableRawMode();
+    char c;
+    while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    }
+    disableRawMode();
     return 0;
 }
