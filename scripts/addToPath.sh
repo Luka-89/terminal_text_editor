a4537a4e89769c5bafba55165396f78e32009848
Luka Batricevic <lukabatricevic8989@gmail.com>
2026-02-26 10:16:45 +0100

    Make addToPath bash script

---
+++ scripts/addToPath.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SCRIPTS_DIR="$(cd "$(dirname "$0")" && pwd)"
+TTE_DIR="$(cd $SCRIPTS_DIR && cd .. && pwd)"
+
+if echo "$PATH" | grep -q "terminal_text_editor/bin"; 
+then
+    echo "It's already in the PATH"
+else
+    echo "export TTE_DIR=\"$TTE_DIR\"" >> ~/.bashrc
+    echo 'export PATH="$PATH:$TTE_DIR/bin"' >> ~/.bashrc
+fi
